6785ebb3b414a20680b80267d953835da05927cf
Ben Gridley <bggridley1@gmail.com>
2024-07-24 18:52:58 -0600

    try again

--- backend/main.go
+++ backend/main.go
@@ -71,6 +71,7 @@ func loadEnvHack() {
 
 	for key, value := range secretsMap {
 		os.Setenv(key, value)
+    fmt.Println("Found a secret", key)
 	}
 
 	fmt.Println("Environment variables loaded from secrets successfully.")
