c42d4fc29631baba7b8a4ae1c0233af955dee5e6
Ben Gridley <bggridley1@gmail.com>
2024-07-24 18:41:59 -0600

    try workaround

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+  "encoding/json"
+  "io/ioutil"
+  "fmt"
+  "os"
 	"github.com/go-playground/validator/v10"
 	"net/http"
 	"backend/config"
@@ -47,4 +51,26 @@ func main() {
 	err := server.ListenAndServe()
 	helper.ErrorPanic(err)
 
+}
+
+// this is strictly because AWS did not want to create secret object in kubernetes...
+func loadEnvHack() {
+  secretsFilePath := "/mnt/secrets-store/realsecrets" // can use a github action secret for this bit later
+
+	secretsData, err := ioutil.ReadFile(secretsFilePath)
+	if err != nil {
+		fmt.Println("Secrets file not found. Exiting.")
+		return
+	}
+	var secretsMap map[string]string
+	if err := json.Unmarshal(secretsData, &secretsMap); err != nil {
+		fmt.Println("Error decoding JSON content. Exiting.")
+		return
+	}
+
+	for key, value := range secretsMap {
+		os.Setenv(key, value)
+	}
+
+	fmt.Println("Environment variables loaded from secrets successfully.")
 }
