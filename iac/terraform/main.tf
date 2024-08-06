ea6defd14c09f0e7ba7cbad9a59809a8e9f0af3a
Ben Gridley <bggridley1@gmail.com>
2024-08-05 20:05:55 -0600

    create container registry

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "test-rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "test-registry"
+  name                = "fullstackloadgen"
   resource_group_name  = azurerm_resource_group.test-rg.name
   location            = azurerm_resource_group.test-rg.location
   sku                 = "Basic"
